78a992d24eb1cdba79eba10d9af5011351320e1e
datawn-mh <kmh102808@data-wn.com>
2024-05-04 14:33:09 +0900

    Red : 자판기 잔액 출력 테스트 케이스 작성

---
+++ src/test/java/vending_machine/VendingMachinePrinterTest.java
@@ -0,0 +1,46 @@
+package vending_machine;
+
+import org.junit.jupiter.api.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class VendingMachinePrinterTest {
+
+    private static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private static final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    private static final PrintStream originalOut = System.out;
+    private static final PrintStream originalErr = System.err;
+
+    @BeforeAll
+    public static void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterAll
+    public static void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+    @Test
+    @DisplayName("자판기의 잔액이 출력된다.")
+    void printBalance() {
+        int money = 1000;
+        VendingMachine machine = getMachine(money);
+        VendingMachinePrinter printer = new VendingMachinePrinter(machine);
+
+        printer.printBalance();
+        assertThat(outContent.toString()).isEqualTo(String.format(VendingMachinePrinter.BALANCE_FORMAT, machine.getBalance()));
+    }
+
+    private VendingMachine getMachine(int money) {
+        VendingMachine machine = new VendingMachine();
+        machine.putMoney(money);
+        return machine;
+    }
+}
