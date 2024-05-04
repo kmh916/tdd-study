e125450113b2d9c12097d418fd49ab1c70423f7c
datawn-mh <kmh102808@data-wn.com>
2024-05-04 16:23:07 +0900

    Red : 자판기 상품 출력 테스트 케이스 작성

--- src/test/java/vending_machine/VendingMachinePrinterTest.java
+++ src/test/java/vending_machine/VendingMachinePrinterTest.java
@@ -1,9 +1,11 @@
 package vending_machine;
 
 import org.junit.jupiter.api.*;
+import vending_machine.product.VendingMachineProduct;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -38,8 +40,41 @@ public class VendingMachinePrinterTest {
         assertThat(outContent.toString()).isEqualTo(String.format(VendingMachinePrinter.BALANCE_FORMAT + "\n", machine.getBalance()));
     }
 
+    @Test
+    @DisplayName("자판기에 상품이 없는 경우 '자판기에 상품이 없습니다.' 메시지가 출력된다. ")
+    void printProducts() {
+        VendingMachinePrinter vendingMachinePrinter = new VendingMachinePrinter(getMachine(10000));
+
+        vendingMachinePrinter.printProducts();
+
+        assertThat(outContent.toString()).isEqualTo(VendingMachinePrinter.PRODUCTS_EMPTY_MESSAGE + "\n");
+
+    }
+
+    @Test
+    @DisplayName("자판기에 상품이 있는 경우 상품 리스트가 출력된다. (구매 가능 상품의 경우 구매 가능 메시지가 함께 출력된다.)")
+    void printProducts() {
+        List<VendingMachineProduct> products = List.of(
+            new VendingMachineProduct("이슬톡톡", 2400),
+            new VendingMachineProduct("내가만든쿠키", 14000)
+        );
+        VendingMachinePrinter vendingMachinePrinter = new VendingMachinePrinter(getMachine(10000, products));
+
+        vendingMachinePrinter.printProducts();
+
+        assertThat(outContent.toString()).contains(products.get(0) + "(구매가능)");
+        assertThat(outContent.toString()).contains(products.get(1).getName());
+
+    }
+
     private VendingMachine getMachine(int money) {
         VendingMachine machine = new VendingMachine();
+        machine.putMoney(money);
+        return machine;
+    }
+
+    private VendingMachine getMachine(int money, List<VendingMachineProduct> products) {
+        VendingMachine machine = new VendingMachine(products);
         machine.putMoney(money);
         return machine;
     }
