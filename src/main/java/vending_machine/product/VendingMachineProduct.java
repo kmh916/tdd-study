3662817a784702efc486261cafb778c2820a557e
kmh916 <kmh102808@gmail.com>
2024-05-04 16:11:05 +0900

    Green : 자판기 상품 기능 구현

---
+++ src/main/java/vending_machine/product/VendingMachineProduct.java
@@ -0,0 +1,23 @@
+package vending_machine.product;
+
+public class VendingMachineProduct {
+
+    private final String name;
+    private final int price;
+
+    public VendingMachineProduct(String name, int price) {
+        if (name == null || name.isBlank() || price <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
--- src/test/java/vending_machine/product/ProductTest.java
+++ src/test/java/vending_machine/product/ProductTest.java
@@ -10,7 +10,7 @@ public class ProductTest {
 
     @Test
     @DisplayName("상품명과 가격으로 상품을 생성한다.")
-    void createProduct() {
+    void createProductWithNameAndPrice() {
         VendingMachineProduct product = new VendingMachineProduct("product1", 700);
 
         assertThat(product.getName()).isEqualTo("product1");
@@ -19,14 +19,14 @@ public class ProductTest {
 
     @Test
     @DisplayName("상품명은 빈 문자열 혹은 null 일 경우 IllegalArgumentException 을 던진다.")
-    void createProduct() {
+    void nameCouldNotBeNullOrBlank() {
         assertThatThrownBy(() -> new VendingMachineProduct(null, 700)).isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> new VendingMachineProduct("", 700)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("상품 가격이 0이하 일 경우 IllegalArgumentException 을 던진다.")
-    void createProduct() {
+    void priceCouldNotBeNegativeOrZero() {
         assertThatThrownBy(() -> new VendingMachineProduct("product", 0)).isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> new VendingMachineProduct("product", -100)).isInstanceOf(IllegalArgumentException.class);
     }
