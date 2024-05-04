fd6bcbfb72e113798706a578b64cd677bfe00871
datawn-mh <kmh102808@data-wn.com>
2024-05-04 15:59:04 +0900

    Red : 자판기 상품 테스트 케이스 작성
    1. 상품명은 빈 문자열 혹은 null 일 경우
    2. 상품 가격이 0이하 일 경우

--- src/test/java/vending_machine/product/ProductTest.java
+++ src/test/java/vending_machine/product/ProductTest.java
@@ -1,10 +1,10 @@
 package vending_machine.product;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ProductTest {
 
@@ -15,6 +15,20 @@ public class ProductTest {
 
         assertThat(product.getName()).isEqualTo("product1");
         assertThat(product.getPrice()).isEqualTo(700);
+    }
+
+    @Test
+    @DisplayName("상품명은 빈 문자열 혹은 null 일 경우 IllegalArgumentException 을 던진다.")
+    void createProduct() {
+        assertThatThrownBy(() -> new VendingMachineProduct(null, 700)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new VendingMachineProduct("", 700)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("상품 가격이 0이하 일 경우 IllegalArgumentException 을 던진다.")
+    void createProduct() {
+        assertThatThrownBy(() -> new VendingMachineProduct("product", 0)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new VendingMachineProduct("product", -100)).isInstanceOf(IllegalArgumentException.class);
     }
 
 }
