dfd0b15b0f6a265f2455bc37392c8a9b3a585e2f
kmh916 <kmh102808@gmail.com>
2024-05-04 14:06:26 +0900

    Red : 자판기 금액 투입 테스트 케이스 작성
    1. 정상 투입 - 기존 잔액이 없을 경우
    2. 정상 투입 - 기존 잔액이 있을 경우
    3. 비정상 투입 - 금액이 올바르지 않을경우

---
+++ src/test/java/vending_machine/VendingMachineTest.java
@@ -0,0 +1,42 @@
+package vending_machine;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class VendingMachineTest {
+    @ParameterizedTest
+    @ValueSource(ints = {100, 500, 1000, 5000, 10000})
+    @DisplayName("자판기에 금액 투입시 정상 금액일 경우 자판기 잔액이 증가한다")
+    void putValidMoneyIntoMachine(int money) {
+        VendingMachine machine = new VendingMachine();
+        machine.putMoney(money);
+
+        assertThat(vendingMachine.getBalance()).isEqualTo(money);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {100, 500, 1000, 5000, 10000})
+    @DisplayName("자판기에 금액 투입시 기존 잔액이 존재 할 경우 잔액은 (잔액 + 투입금액) 이다.")
+    void putValidMoneyIntoMachineWhenExistsBalance(int money) {
+        int balance = 100;
+        VendingMachine machine = new VendingMachine();
+
+        machine.putMoney(balance);
+        machine.putMoney(money);
+
+        assertThat(vendingMachine.getBalance()).isEqualTo(money + balance);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {157, 665, 191283})
+    @DisplayName("자판기에 올바르지 않은 금액을 입력할 경우 IllegalArgumentException을 던진다.")
+    void putInvalidMoney(int money) {
+        VendingMachine machine = new VendingMachine();
+
+        assertThatThrownBy(() -> machine.putMoney(money)).isInstanceOf(IllegalArgumentException.class);
+    }
+}
