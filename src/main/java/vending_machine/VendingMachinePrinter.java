d77446b8ef69c9f4df078aba188f9984f8b4e4df
kmh916 <kmh102808@gmail.com>
2024-05-06 14:58:51 +0900

    Refactor : 자판기 상품 출력 리팩토링

--- src/main/java/vending_machine/VendingMachinePrinter.java
+++ src/main/java/vending_machine/VendingMachinePrinter.java
@@ -32,11 +32,7 @@ public class VendingMachinePrinter {
         System.out.println(PRODUCTS_HEADER);
         for (int i = 0; i < products.size(); i++) {
             VendingMachineProduct product = products.get(i);
-            String ableMessage = "";
-            if (product.getPrice() <= this.machine.getBalance()) {
-                ableMessage = "(구매가능)";
-            }
-
+            String ableMessage = product.getPrice() <= this.machine.getBalance() ? "(구매가능)" : "";
             System.out.printf("%s : %s%s\n", (i + 1), product, ableMessage);
         }
 
